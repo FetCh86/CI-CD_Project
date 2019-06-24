fa446b40ce34860b4d0b1a71638b18b4e3dd1d04
Fethi <fchouder86@gmail.com>
2019-06-24 14:49:02 +0200

    tests php unit added

---
+++ src/Service/Calculator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fchou
+ * Date: 24/06/2019
+ * Time: 14:30
+ */
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+
+class Calculator
+{
+    public function add(int $firstNumber, int $secondNumber): int
+    {
+        return $firstNumber + $secondNumber;
+    }
+}
